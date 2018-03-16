5677752cdb51dd80a472d954956ab705296f3413
Volker Hirthammer <volker.hirthammer@gmail.com>
2018-03-16 15:22:57 +0100

    Change cmake compiler setup

--- scripts/cmake/CreateSingleSourceObjectLib.cmake
+++ scripts/cmake/CreateSingleSourceObjectLib.cmake
@@ -5,6 +5,8 @@ function(CreateSingleSourceObjectLib Source Object)
     add_library(${libName} OBJECT ${Source})
     ### Add src directory
     target_include_directories(${libName} PUBLIC ${CMAKE_SOURCE_DIR}/src)
+    ### Add compile features
+    target_compile_features(${libName} PUBLIC cxx_std_17)
     ### Make target global
     set(${Object} "$<TARGET_OBJECTS:${libName}>" PARENT_SCOPE)
 endfunction()
