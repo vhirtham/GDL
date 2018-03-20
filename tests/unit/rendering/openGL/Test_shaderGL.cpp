350e95941fd222f5f24cd365f48e2ab04e242783
Volker Hirthammer <volker.hirthammer@gmail.com>
2018-03-20 18:01:34 +0100

    Enhance shaderGL test

--- tests/unit/rendering/openGL/Test_shaderGL.cpp
+++ tests/unit/rendering/openGL/Test_shaderGL.cpp
@@ -6,10 +6,21 @@
 
 using namespace GDL;
 
-BOOST_AUTO_TEST_CASE(renderWindow)
+BOOST_AUTO_TEST_CASE(shaderGL)
 {
     RenderWindowGL renderWindow;
     renderWindow.Initialize();
 
     BOOST_CHECK_THROW(ShaderGL shader(GL_VERTEX_SHADER, "fail"), Exception);
+
+    const char vertexShaderCode[] = R"glsl(
+                                    #version 430
+                                    layout(location=0) in vec4 in_Position;
+                                    void main(void)
+                                    {
+                                        gl_Position = in_Position;
+                                    }
+                                    )glsl";
+
+    ShaderGL shader2(GL_VERTEX_SHADER, vertexShaderCode);
 }
