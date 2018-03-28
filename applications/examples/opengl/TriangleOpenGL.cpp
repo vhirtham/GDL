7ccadcb3da02d57fc9eaa68c18b07f5a7c85d9be
Volker Hirthammer <volker.hirthammer@gmail.com>
2018-03-28 20:52:22 +0200

    Try some things concerning allocators

--- applications/examples/opengl/TriangleOpenGL.cpp
+++ applications/examples/opengl/TriangleOpenGL.cpp
@@ -104,11 +104,11 @@ void CreateVBO(void)
 
     GLenum ErrorCheckValue = glGetError();
 
+    glGenBuffers(1, &VboId);
+    glBindBuffer(GL_ARRAY_BUFFER, VboId);
     glGenVertexArrays(1, &VaoId);
     glBindVertexArray(VaoId);
 
-    glGenBuffers(1, &VboId);
-    glBindBuffer(GL_ARRAY_BUFFER, VboId);
     glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
     glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, 0);
     glEnableVertexAttribArray(0);
--- tests/unit/resources/Test_Allocator.cpp
+++ tests/unit/resources/Test_Allocator.cpp
@@ -2,6 +2,107 @@
 #include "gdl/resources/memory/PoolAllocator.h"
 #include <vector>
 
+#include <new>
+
+class GlobalNewAllocationCounter
+{
+    friend void* operator new(std::size_t size);
+    friend void* operator new(std::size_t size, const std::nothrow_t&) noexcept;
+    friend void* operator new[](std::size_t size);
+    friend void* operator new[](std::size_t size, const std::nothrow_t&) noexcept;
+    friend void operator delete(void* ptr) noexcept;
+    friend void operator delete(void* ptr, const std::nothrow_t&)noexcept;
+    friend void operator delete[](void* ptr) noexcept;
+    friend void operator delete[](void* ptr, const std::nothrow_t&) noexcept;
+
+    int mAllocationsOnConstruction = -1;
+    int mDeallocationsOnConstruction = -1;
+
+    static int mTotalAllocations;
+    static int mTotalDeallocations;
+
+public:
+    GlobalNewAllocationCounter()
+        : mAllocationsOnConstruction(mTotalAllocations)
+        , mDeallocationsOnConstruction(mTotalDeallocations)
+    {
+    }
+
+    int GetNumAllocations() const
+    {
+        return mTotalAllocations - mAllocationsOnConstruction;
+    }
+    int GetNumDeallocations() const
+    {
+        return mTotalDeallocations - mDeallocationsOnConstruction;
+    }
+
+    static int GetTotalNumAllocations()
+    {
+        return mTotalAllocations;
+    }
+    static int GetTotalNumDeallocations()
+    {
+        return mTotalDeallocations;
+    }
+};
+
+int GlobalNewAllocationCounter::mTotalAllocations = 0;
+int GlobalNewAllocationCounter::mTotalDeallocations = 0;
+
+void* operator new(std::size_t size)
+{
+    ++GlobalNewAllocationCounter::mTotalAllocations;
+    void* p = malloc(size);
+    if (!p)
+        throw std::bad_alloc();
+    return p;
+}
+
+void* operator new[](std::size_t size)
+{
+    ++GlobalNewAllocationCounter::mTotalAllocations;
+    void* p = malloc(size);
+    if (!p)
+        throw std::bad_alloc();
+    return p;
+}
+
+void* operator new[](std::size_t size, const std::nothrow_t&) noexcept
+{
+    ++GlobalNewAllocationCounter::mTotalAllocations;
+    return malloc(size);
+}
+void* operator new(std::size_t size, const std::nothrow_t&) noexcept
+{
+    ++GlobalNewAllocationCounter::mTotalAllocations;
+    return malloc(size);
+}
+
+
+void operator delete(void* ptr) noexcept
+{
+    ++GlobalNewAllocationCounter::mTotalDeallocations;
+    free(ptr);
+}
+void operator delete(void* ptr, const std::nothrow_t&)noexcept
+{
+    ++GlobalNewAllocationCounter::mTotalDeallocations;
+    free(ptr);
+}
+void operator delete[](void* ptr) noexcept
+{
+    ++GlobalNewAllocationCounter::mTotalDeallocations;
+    free(ptr);
+}
+void operator delete[](void* ptr, const std::nothrow_t&) noexcept
+{
+    ++GlobalNewAllocationCounter::mTotalDeallocations;
+    free(ptr);
+}
+
+
+
 struct test
 {
     int mA;
@@ -13,35 +114,71 @@ struct test
     }
 };
 
+template <template <typename> class Allocator = std::allocator>
+struct test2
+{
+    std::vector<int, Allocator<int>> mVec;
+};
+
+
+
 using namespace GDL;
 // BOOST_AUTO_TEST_CASE(PoolAllocator)
 int main()
 {
+    //    int allocs = number_of_allocs;
     MemoryManager::Instance(1024);
-
-    std::vector<test, GDL::custom_allocator<test>> v;
-    std::vector<F32, GDL::custom_allocator<F32>> v2;
-
-    v.push_back(42);
-    v2.push_back(3.14);
-    v.push_back(56);
-    v.push_back(11);
-    v.push_back(22);
-    v.reserve(20);
-    bool caught = false;
-    try
+    GlobalNewAllocationCounter ac;
+    std::cout << "Num allocations start: " << ac.GetNumAllocations() << std::endl;
     {
-        v.reserve(200);
-    }
-    catch (Exception e)
-    {
-        e.what();
-        caught = true;
-    }
+        std::cout << "Num allocations CP 1: " << ac.GetNumAllocations() << std::endl;
 
-    if (caught == false)
-        throw Exception(__PRETTY_FUNCTION__, "Exception not caught as expected");
-    std::cout << "End of program" << std::endl;
-    //    v.push_back(33);
-    //    v.push_back(44);
+        std::vector<test, GDL::custom_allocator<test>> v;
+        std::vector<test2<GDL::custom_allocator>, GDL::custom_allocator<test2<GDL::custom_allocator>>> v3;
+        std::vector<F32, GDL::custom_allocator<F32>> v2;
+
+        v.push_back(42);
+        v2.push_back(3.14);
+        v.push_back(56);
+        v.push_back(11);
+        v.push_back(22);
+        v.reserve(20);
+        v3.emplace_back();
+        v3.back().mVec.push_back(1);
+        v3.back().mVec.push_back(2);
+        v3.emplace_back(v3.back());
+
+        test2 test;
+        std::cout << "Num allocations CP 2: " << ac.GetNumAllocations() << std::endl;
+        test.mVec.push_back(42);
+        std::cout << "Num allocations CP 3: " << ac.GetNumAllocations() << std::endl;
+        int* a1 = new int(10);
+        std::cout << "Num allocations CP new a1: " << ac.GetNumAllocations() << std::endl;
+        std::cout << "Num deallocations CP new a1: " << ac.GetNumDeallocations() << std::endl;
+        delete a1;
+        std::cout << "Num allocations CP delete a1: " << ac.GetNumAllocations() << std::endl;
+        std::cout << "Num deallocations CP delete a1: " << ac.GetNumDeallocations() << std::endl;
+
+        bool caught = false;
+        //    try
+        //    {
+        //        v.reserve(200);
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        e.what();
+        //        caught = true;
+        //    }
+
+        //    if (caught == false)
+        //        throw Exception(__PRETTY_FUNCTION__, "Exception not caught as expected");
+        //    std::cout << "End of program" << std::endl;
+        //    v.push_back(33);
+        //    v.push_back(44);
+    }
+    std::cout << "Num allocations CP END: " << ac.GetNumAllocations() << std::endl;
+    std::cout << "Num deallocations CP END: " << ac.GetNumDeallocations() << std::endl;
+    std::cout << "Num GLOBAL allocations CP END: " << GlobalNewAllocationCounter::GetTotalNumAllocations() << std::endl;
+    std::cout << "Num GLOBAL deallocations CP END: " << GlobalNewAllocationCounter::GetTotalNumDeallocations()
+              << std::endl;
 }
