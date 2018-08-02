6042b58f331f6a4ee6d76a2d114e09037345aa38
Volker Hirthammer <volker.hirthammer@gmail.com>
2018-08-02 11:56:43 +0200

    Generalize allocator tests

--- tests/unit/resources/memory/Test_allocator.cpp
+++ tests/unit/resources/memory/Test_allocator.cpp
@@ -12,17 +12,21 @@
 using namespace GDL;
 
 
-// template <template <typename> class Allocator = std::allocator>
-// struct test2
-//{
-//    std::vector<int, Allocator<int>> mVec;
-//};
+template <template <typename> class _allocator>
+void VectorTest()
+{
+    constexpr U32 numElements = 100;
 
-// template <typename TAllocator>
-// void someFunc(const std::vector<GDL::F32, TAllocator>& A)
-//{
-//    std::cout << "can do it" << std::endl;
-//}
+    std::vector<I32, _allocator<I32>> v;
+
+    for (U32 i = 0; i < numElements; ++i)
+        v.push_back(static_cast<I32>(i) * 2);
+    for (U32 i = 0; i < numElements; ++i)
+        BOOST_CHECK(v[i] == static_cast<I32>(i) * 2);
+
+    v.clear();
+    v.shrink_to_fit();
+}
 
 
 BOOST_AUTO_TEST_CASE(Vector)
@@ -30,35 +34,39 @@ BOOST_AUTO_TEST_CASE(Vector)
     auto& mM = GetMemoryManager();
     mM.Initialize();
 
-    std::vector<F32, GeneralPurposeAllocator<F32>> vec;
-    vec.resize(7);
-    for (U32 i = 0; i < vec.size(); ++i)
-        vec[i] = i * 2;
-    for (U32 i = 0; i < vec.size(); ++i)
-        std::cout << vec[i] << std::endl;
-    vec.clear();
-    vec.shrink_to_fit();
+    BOOST_CHECK_NO_THROW(VectorTest<GeneralPurposeAllocator>());
 
     mM.Deinitialize();
 }
 
 
-BOOST_AUTO_TEST_CASE(Map)
+
+template <template <typename> class _allocator>
+void MapTest()
 {
-    auto& mM = MemoryManager::Instance();
-    mM.Initialize();
+    constexpr U32 numElements = 100;
 
-    std::cout << sizeof(std::pair<const I32, F32>) << std::endl;
+    std::map<U32, I32, std::less<U32>, _allocator<std::pair<const U32, I32>>> m;
 
-    std::map<I32, F32, std::less<I32>, GeneralPurposeAllocator<std::pair<const I32, F32>>> m;
+    for (U32 i = 0; i < numElements; ++i)
+        m.emplace(i, static_cast<I32>(i));
 
-    for (I32 i = 0; i < 20; ++i)
-        m.emplace(i, static_cast<F32>(i));
-
-    for (I32 i = 0; i < 20; ++i)
-        BOOST_CHECK(m.find(i) != m.end());
+    for (U32 i = 0; i < numElements; ++i)
+    {
+        const auto& entry = m.find(i);
+        BOOST_CHECK(entry != m.end());
+        BOOST_CHECK(entry->second == static_cast<I32>(i));
+    }
 
     m.clear();
+}
+
+BOOST_AUTO_TEST_CASE(Map)
+{
+    auto& mM = GetMemoryManager();
+    mM.Initialize();
+
+    BOOST_CHECK_NO_THROW(MapTest<GeneralPurposeAllocator>());
 
     mM.Deinitialize();
 }
