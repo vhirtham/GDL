17a0ecb0975cfb620f8a2d4d9c1daf9cd5549d7d
Volker Hirthammer <volker.hirthammer@gmail.com>
2018-08-16 13:07:00 +0200

    Simplify exception class

--- gdl/base/Exception.h
+++ gdl/base/Exception.h
@@ -4,6 +4,9 @@
 #include <stdexcept>
 
 
+
+// Macros -------------------------------------------------------------------------------------------------------------
+
 #define EXCEPTION(condition, message)                                                                                  \
     if (condition)                                                                                                     \
     throw GDL::Exception(__PRETTY_FUNCTION__, message)
@@ -24,74 +27,31 @@
 #define DEV_EXCEPTION(condition, message)
 #endif
 
+
+
+// Class definition ---------------------------------------------------------------------------------------------------
+
 namespace GDL
 {
 
 //! @brief Stores origion and message of an exception
-class Exception : public std::exception
+class Exception : public std::runtime_error
 {
-    std::string mOrigin; //!< Name of the function that caused the exception
-    std::string mMessage; //!< Exception message
-    std::string mFullMessage; //!< Exception message including the origin
-
 
 public:
-    //! @brief Constructor
     Exception() = delete;
-
-    //! @brief Copy constructor
-    //! @param other: Object that should be copied
     Exception(const Exception& other) = default;
-
-    //! @brief Move constructor
-    //! @param other: Object that should be moved
     Exception(Exception&& other) = default;
-
-    //! @brief Copy assignment operator
-    //! @param other: Object that should be copied
     Exception& operator=(const Exception& other) = default;
-
-    //! @brief Move assignment operator
-    //! @param other: Object that should be moved
     Exception& operator=(Exception&& other) = default;
-
-    //! @brief Destructor
-    ~Exception() = default;
+    virtual ~Exception() = default;
 
     //! @brief Constructor
     //! @param origin: Name of the function that caused the exception
     //! @param message: Exception message
     Exception(std::string origin, std::string message)
-        : mOrigin(origin)
-        , mMessage(message)
-        , mFullMessage("[" + origin + "] " + message)
+        : std::runtime_error{"[" + origin + "] " + message}
     {
-    }
-
-
-    //! @brief Gets the full exception message including the origin
-    const std::string& getFullMessage() const
-    {
-        return mFullMessage;
-    }
-
-    //! @brief Gets the message of the exception
-    const std::string& getMessage() const
-    {
-        return mMessage;
-    }
-
-    //! @brief Gets the origin of the exception
-    const std::string& getOrigin() const
-    {
-        return mOrigin;
-    }
-
-
-    //! @brief Gets the full exception message including the origin
-    virtual const char* what() const noexcept override
-    {
-        return getFullMessage().c_str();
     }
 };
 }
