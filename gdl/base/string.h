cb371d6a5fc3bc23392058706aadc0fe40c1f841
Volker Hirthammer <volker.hirthammer@gmail.com>
2018-09-12 10:29:09 +0200

    Add integer variants of ToString and test them

--- gdl/base/string.h
+++ gdl/base/string.h
@@ -37,20 +37,147 @@ using StringTPS = std::basic_string<char>;
 
 namespace GDL
 {
+
+
+// ToString functions -------------------------------------------------------------------------------------------------
+
+
+
+//! @brief Converts an F32 number into a human readable string
+//! @tparam _bufferSize: Size of the temporary char buffer. Use the default value unless memory size is critical.
+//! @param value: Number that should be converted
+//! @param format: Char array to specify the output format. Have a look at the std::snprintf documentation.
 template <U32 _bufferSize = std::numeric_limits<F32>::max_exponent10 + 20>
-String ToString(F32 value, const char* format = "%.4e")
+String ToString(F32 value, const char* format = "%.4f")
 {
     std::array<char, _bufferSize> buffer;
     std::snprintf(buffer.data(), buffer.size(), format, value);
     return String{buffer.data()};
 }
 
+
+
+//! @brief Converts an F64 number into a human readable string
+//! @tparam _bufferSize: Size of the temporary char buffer. Use the default value unless memory size is critical.
+//! @param value: Number that should be converted
+//! @param format: Char array to specify the output format. Have a look at the std::snprintf documentation.
 template <U32 _bufferSize = std::numeric_limits<F64>::max_exponent10 + 20>
-String ToString(F64 value, const char* format = "%.4e")
+String ToString(F64 value, const char* format = "%.4f")
 {
     std::array<char, _bufferSize> buffer;
     std::snprintf(buffer.data(), buffer.size(), format, value);
     return String{buffer.data()};
 }
 
+
+
+//! @brief Converts an I8 number into a human readable string
+//! @tparam _bufferSize: Size of the temporary char buffer. Use the default value unless memory size is critical.
+//! @param value: Number that should be converted
+//! @param format: Char array to specify the output format. Have a look at the std::snprintf documentation.
+template <U32 _bufferSize = 8>
+String ToString(I8 value, const char* format = "%hhi")
+{
+    std::array<char, _bufferSize> buffer;
+    std::snprintf(buffer.data(), buffer.size(), format, value);
+    return String{buffer.data()};
+}
+
+
+
+//! @brief Converts an I16 number into a human readable string
+//! @tparam _bufferSize: Size of the temporary char buffer. Use the default value unless memory size is critical.
+//! @param value: Number that should be converted
+//! @param format: Char array to specify the output format. Have a look at the std::snprintf documentation.
+template <U32 _bufferSize = 8>
+String ToString(I16 value, const char* format = "%hi")
+{
+    std::array<char, _bufferSize> buffer;
+    std::snprintf(buffer.data(), buffer.size(), format, value);
+    return String{buffer.data()};
+}
+
+
+
+//! @brief Converts an I32 number into a human readable string
+//! @tparam _bufferSize: Size of the temporary char buffer. Use the default value unless memory size is critical.
+//! @param value: Number that should be converted
+//! @param format: Char array to specify the output format. Have a look at the std::snprintf documentation.
+template <U32 _bufferSize = 16>
+String ToString(I32 value, const char* format = "%i")
+{
+    std::array<char, _bufferSize> buffer;
+    std::snprintf(buffer.data(), buffer.size(), format, value);
+    return String{buffer.data()};
+}
+
+
+
+//! @brief Converts an I64 number into a human readable string
+//! @tparam _bufferSize: Size of the temporary char buffer. Use the default value unless memory size is critical.
+//! @param value: Number that should be converted
+//! @param format: Char array to specify the output format. Have a look at the std::snprintf documentation.
+template <U32 _bufferSize = 32>
+String ToString(I64 value, const char* format = "%li")
+{
+    std::array<char, _bufferSize> buffer;
+    std::snprintf(buffer.data(), buffer.size(), format, value);
+    return String{buffer.data()};
+}
+
+
+
+//! @brief Converts an U8 number into a human readable string
+//! @tparam _bufferSize: Size of the temporary char buffer. Use the default value unless memory size is critical.
+//! @param value: Number that should be converted
+//! @param format: Char array to specify the output format. Have a look at the std::snprintf documentation.
+template <U32 _bufferSize = 8>
+String ToString(U8 value, const char* format = "%hhu")
+{
+    std::array<char, _bufferSize> buffer;
+    std::snprintf(buffer.data(), buffer.size(), format, value);
+    return String{buffer.data()};
+}
+
+
+
+//! @brief Converts an U16 number into a human readable string
+//! @tparam _bufferSize: Size of the temporary char buffer. Use the default value unless memory size is critical.
+//! @param value: Number that should be converted
+//! @param format: Char array to specify the output format. Have a look at the std::snprintf documentation.
+template <U32 _bufferSize = 8>
+String ToString(U16 value, const char* format = "%hu")
+{
+    std::array<char, _bufferSize> buffer;
+    std::snprintf(buffer.data(), buffer.size(), format, value);
+    return String{buffer.data()};
+}
+
+
+
+//! @brief Converts an U32 number into a human readable string
+//! @tparam _bufferSize: Size of the temporary char buffer. Use the default value unless memory size is critical.
+//! @param value: Number that should be converted
+//! @param format: Char array to specify the output format. Have a look at the std::snprintf documentation.
+template <U32 _bufferSize = 16>
+String ToString(U32 value, const char* format = "%u")
+{
+    std::array<char, _bufferSize> buffer;
+    std::snprintf(buffer.data(), buffer.size(), format, value);
+    return String{buffer.data()};
+}
+
+
+
+//! @brief Converts an U64 number into a human readable string
+//! @tparam _bufferSize: Size of the temporary char buffer. Use the default value unless memory size is critical.
+//! @param value: Number that should be converted
+//! @param format: Char array to specify the output format. Have a look at the std::snprintf documentation.
+template <U32 _bufferSize = 32>
+String ToString(U64 value, const char* format = "%lu")
+{
+    std::array<char, _bufferSize> buffer;
+    std::snprintf(buffer.data(), buffer.size(), format, value);
+    return String{buffer.data()};
+}
 } // namespace GDL
--- tests/unit/base/Test_string.cpp
+++ tests/unit/base/Test_string.cpp
@@ -17,12 +17,50 @@ BOOST_AUTO_TEST_CASE(To_String)
     mm.Initialize();
     HeapAllocationCounter hac;
     {
-        std::cout << ToString(3.15678f, "%7.2f") << ";" << std::endl;
-        std::cout << ToString(-10030500000000001.) << ";" << std::endl;
-        std::cout << ToString(0.0000030500000000001) << ";" << std::endl;
-        std::cout << ToString(10030500000000000.f, "%7.5e") << ";" << std::endl;
-        String test("A number = " + ToString(3.156e6f));
-        std::cout << test << ";" << std::endl;
+        // floating point types
+        F32 f32 = -135.2398f;
+        BOOST_CHECK(ToString(std::abs(f32)).compare("135.2398") == 0);
+        BOOST_CHECK(ToString(f32).compare("-135.2398") == 0);
+
+        F64 f64 = -951463784994.1234;
+        BOOST_CHECK(ToString(std::abs(f64)).compare("951463784994.1234") == 0);
+        BOOST_CHECK(ToString(f64).compare("-951463784994.1234") == 0);
+
+
+        // integers
+        I8 i8 = static_cast<I8>(-121);
+        BOOST_CHECK(ToString(static_cast<I8>(std::abs(i8))).compare("121") == 0);
+        BOOST_CHECK(ToString(i8).compare("-121") == 0);
+
+        I16 i16 = static_cast<I16>(-25398);
+        BOOST_CHECK(ToString(static_cast<I16>(std::abs(i16))).compare("25398") == 0);
+        BOOST_CHECK(ToString(i16).compare("-25398") == 0);
+
+        I32 i32 = -1958501683;
+        BOOST_CHECK(ToString(std::abs(i32)).compare("1958501683") == 0);
+        BOOST_CHECK(ToString(i32).compare("-1958501683") == 0);
+
+        I64 i64 = -1207927512116994732;
+        BOOST_CHECK(ToString(std::abs(i64)).compare("1207927512116994732") == 0);
+        BOOST_CHECK(ToString(i64).compare("-1207927512116994732") == 0);
+
+
+
+        // unsigned integers
+        U8 u8 = static_cast<U8>(215);
+        BOOST_CHECK(ToString(u8).compare("215") == 0);
+
+        U16 u16 = static_cast<U16>(55398);
+        BOOST_CHECK(ToString(u16).compare("55398") == 0);
+
+        U32 u32 = 3958501683;
+        BOOST_CHECK(ToString(u32).compare("3958501683") == 0);
+
+        U64 u64 = 3207927512116994732;
+        BOOST_CHECK(ToString(u64).compare("3207927512116994732") == 0);
+
+        std::cout << ToString(i64) << ";" << std::endl;
+
         std::to_string(2);
     }
     hac.PrintCalls();
