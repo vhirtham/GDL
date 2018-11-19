819c43d0664d927e5710ab2482dbdaa9e097db70
Volker Hirthammer <volker.hirthammer@gmail.com>
2018-11-19 23:33:24 +0100

    Test exceptions of sse::GetValue

--- test/unit/base/sse/Test_directAccess.cpp
+++ test/unit/base/sse/Test_directAccess.cpp
@@ -12,7 +12,7 @@ BOOST_AUTO_TEST_CASE(Get_Value)
     BOOST_CHECK(sse::GetValue<1>(d128) == 2.);
     BOOST_CHECK(sse::GetValue(d128, 0) == 1.);
     BOOST_CHECK(sse::GetValue(d128, 1) == 2.);
-
+    BOOST_CHECK_THROW(sse::GetValue(d128, 2), Exception);
 
     __m128 f128 = _mm_setr_ps(1.f, 2.f, 3.f, 4.f);
     BOOST_CHECK(sse::GetValue<0>(f128) == 1.f);
@@ -23,6 +23,8 @@ BOOST_AUTO_TEST_CASE(Get_Value)
     BOOST_CHECK(sse::GetValue(f128, 1) == 2.f);
     BOOST_CHECK(sse::GetValue(f128, 2) == 3.f);
     BOOST_CHECK(sse::GetValue(f128, 3) == 4.f);
+    BOOST_CHECK_THROW(sse::GetValue(f128, 4), Exception);
+
 
 #ifdef __AVX2__
     __m256d d256 = _mm256_setr_pd(1., 2., 3., 4.);
@@ -34,7 +36,7 @@ BOOST_AUTO_TEST_CASE(Get_Value)
     BOOST_CHECK(sse::GetValue(d256, 1) == 2.);
     BOOST_CHECK(sse::GetValue(d256, 2) == 3.);
     BOOST_CHECK(sse::GetValue(d256, 3) == 4.);
-
+    BOOST_CHECK_THROW(sse::GetValue(d256, 4), Exception);
 
     __m256 f256 = _mm256_setr_ps(1.f, 2.f, 3.f, 4.f, 5.f, 6.f, 7.f, 8.f);
     BOOST_CHECK(sse::GetValue<0>(f256) == 1.f);
@@ -53,5 +55,6 @@ BOOST_AUTO_TEST_CASE(Get_Value)
     BOOST_CHECK(sse::GetValue(f256, 5) == 6.f);
     BOOST_CHECK(sse::GetValue(f256, 6) == 7.f);
     BOOST_CHECK(sse::GetValue(f256, 7) == 8.f);
+    BOOST_CHECK_THROW(sse::GetValue(f256, 8), Exception);
 #endif // __AVX2__
 }
