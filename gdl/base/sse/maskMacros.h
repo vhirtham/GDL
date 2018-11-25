05e60ed99dc49c691dd5ec9fff38d6d3a5b0a738
Volker Hirthammer <volker.hirthammer@gmail.com>
2018-11-25 16:20:29 +0100

    Add sse blend macro

--- gdl/base/sse/maskMacros.h
+++ gdl/base/sse/maskMacros.h
@@ -1,5 +1,8 @@
 #pragma once
 
 
-#define GDL_SHUFFLE2(a, b) (((b) << 1) | (a))
-#define GDL_SHUFFLE4(a, b, c, d) (((d) << 6) | ((c) << 4) | ((b) << 2) | (a))
+#define SHUFFLE_MASK_2(a, b) (((b) << 1) | (a))
+#define SHUFFLE_MASK_4(a, b, c, d) (((d) << 6) | ((c) << 4) | ((b) << 2) | (a))
+
+#define BLEND_MASK_2(a, b) (((b) << 1) | (a))
+#define BLEND_MASK_4(a, b, c, d) (((d) << 3) | ((c) << 2) | ((b) << 1) | (a))
--- test/unit/base/sse/Test_maskMacros.cpp
+++ test/unit/base/sse/Test_maskMacros.cpp
@@ -8,13 +8,19 @@
 
 using namespace GDL;
 
+#include <iostream>
+
+
+
+// Shuffle 2 ----------------------------------------------------------------------------------------------------------
+
 template <U32 _i = 0, U32 _j = 0>
 void TestShuffle2()
 {
     const __m128d a = _mm_setr_pd(1., 2.);
     const __m128d b = _mm_setr_pd(3., 4.);
 
-    const __m128d c = _mm_shuffle_pd(a, b, GDL_SHUFFLE2(_i, _j));
+    const __m128d c = _mm_shuffle_pd(a, b, SHUFFLE_MASK_2(_i, _j));
 
     BOOST_CHECK(sse::GetValue<0>(c) == Approx(sse::GetValue<_i>(a)));
     BOOST_CHECK(sse::GetValue<1>(c) == Approx(sse::GetValue<_j>(b)));
@@ -28,6 +34,7 @@ void TestShuffle2()
 }
 
 
+
 BOOST_AUTO_TEST_CASE(Shuffle2)
 {
     TestShuffle2();
@@ -35,13 +42,15 @@ BOOST_AUTO_TEST_CASE(Shuffle2)
 
 
 
+// Shuffle 4 ----------------------------------------------------------------------------------------------------------
+
 template <U32 _i = 0, U32 _j = 0, U32 _k = 0, U32 _l = 0>
 void TestShuffle4()
 {
     const __m128 a = _mm_setr_ps(1.f, 2.f, 3.f, 4.f);
     const __m128 b = _mm_setr_ps(5.f, 6.f, 7.f, 8.f);
 
-    const __m128 c = _mm_shuffle_ps(a, b, GDL_SHUFFLE4(_i, _j, _k, _l));
+    const __m128 c = _mm_shuffle_ps(a, b, SHUFFLE_MASK_4(_i, _j, _k, _l));
 
 
 
@@ -61,7 +70,74 @@ void TestShuffle4()
     }
 }
 
+
+
 BOOST_AUTO_TEST_CASE(Shuffle4)
 {
     TestShuffle4();
+}
+
+
+
+// Blend 2 ------------------------------------------------------------------------------------------------------------
+
+template <U32 _i = 0, U32 _j = 0>
+void TestBlend2()
+{
+    const __m128d a = _mm_setr_pd(1., 2.);
+    const __m128d b = _mm_setr_pd(3., 4.);
+
+    const __m128d c = _mm_blend_pd(a, b, BLEND_MASK_2(_i, _j));
+
+    BOOST_CHECK(sse::GetValue<0>(c) == Approx((_i == 0) ? 1.f : 3.f));
+    BOOST_CHECK(sse::GetValue<1>(c) == Approx((_j == 0) ? 2.f : 4.f));
+
+    constexpr U32 iNext = (_i < 1) ? _i + 1 : 0;
+    constexpr U32 jNext = (iNext == 0) ? _j + 1 : _j;
+    if constexpr (jNext < 2)
+    {
+        TestBlend2<iNext, jNext>();
+    }
+}
+
+
+BOOST_AUTO_TEST_CASE(Blend2)
+{
+    TestBlend2();
+}
+
+
+
+// Blend 4 ------------------------------------------------------------------------------------------------------------
+
+template <U32 _i = 0, U32 _j = 0, U32 _k = 0, U32 _l = 0>
+void TestBlend4()
+{
+    const __m128 a = _mm_setr_ps(1.f, 2.f, 3.f, 4.f);
+    const __m128 b = _mm_setr_ps(5.f, 6.f, 7.f, 8.f);
+
+    const __m128 c = _mm_blend_ps(a, b, BLEND_MASK_4(_i, _j, _k, _l));
+
+
+    BOOST_CHECK(sse::GetValue<0>(c) == Approx((_i == 0) ? 1.f : 5.f));
+    BOOST_CHECK(sse::GetValue<1>(c) == Approx((_j == 0) ? 2.f : 6.f));
+    BOOST_CHECK(sse::GetValue<2>(c) == Approx((_k == 0) ? 3.f : 7.f));
+    BOOST_CHECK(sse::GetValue<3>(c) == Approx((_l == 0) ? 4.f : 8.f));
+
+
+    constexpr U32 iNext = (_i < 1) ? _i + 1 : 0;
+    constexpr U32 jNext = (iNext == 0) ? (_j < 1) ? _j + 1 : 0 : _j;
+    constexpr U32 kNext = (iNext == 0 && jNext == 0) ? (_k < 1) ? _k + 1 : 0 : _k;
+    constexpr U32 lNext = (iNext == 0 && jNext == 0 && kNext == 0) ? _l + 1 : _l;
+    if constexpr (lNext < 2)
+    {
+        TestBlend4<iNext, jNext, kNext, lNext>();
+    }
+}
+
+
+
+BOOST_AUTO_TEST_CASE(Blend4)
+{
+    TestBlend4();
 }
