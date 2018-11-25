41216f28031518ec0a5275526e0e3454b04b5283
Volker Hirthammer <volker.hirthammer@gmail.com>
2018-11-25 18:31:02 +0100

    Add sse permute mask

--- gdl/base/sse/maskMacros.h
+++ gdl/base/sse/maskMacros.h
@@ -1,8 +1,33 @@
 #pragma once
 
 
+//! @brief Creates a mask for shuffle intrinsics
+//! @param a: Index of a value of the first source register that is used as first result value
+//! @param b: Index of a value of the second source register that is used as second result value
 #define SHUFFLE_MASK_2(a, b) (((b) << 1) | (a))
+
+//! @brief Creates a mask for shuffle intrinsics
+//! @param a: Index of a value of the first source register that is used as first result value
+//! @param b: Index of a value of the first source register that is used as second result value
+//! @param c: Index of a value of the second source register that is used as third result value
+//! @param d: Index of a value of the second source register that is used as fourth result value
 #define SHUFFLE_MASK_4(a, b, c, d) (((d) << 6) | ((c) << 4) | ((b) << 2) | (a))
 
+//! @brief Creates a mask for blend intrinsics
+//! @param a: First result value is taken from: 0 = first register, 1 = second register
+//! @param b: Second result value is taken from: 0 = first register, 1 = second register
 #define BLEND_MASK_2(a, b) (((b) << 1) | (a))
+
+//! @brief Creates a mask for blend intrinsics
+//! @param a: First result value is taken from: 0 = first register, 1 = second register
+//! @param b: Second result value is taken from: 0 = first register, 1 = second register
+//! @param c: Third result value is taken from: 0 = first register, 1 = second register
+//! @param d: Fourth result value is taken from: 0 = first register, 1 = second register
 #define BLEND_MASK_4(a, b, c, d) (((d) << 3) | ((c) << 2) | ((b) << 1) | (a))
+
+//! @brief Creates a mask for the _mm256_permute2f128 intrinsics
+//! @param r0: Source register index for the lower 128bits of the result (0 = first / 1 = second of passed registers)
+//! @param v0: Value index for the lower 128bits of the result (0 = higher / 1 = lower 128 bits of source register)
+//! @param r1: Source register index for the higher 128bits of the result (0 = first / 1 = second of passed registers)
+//! @param v1: Value index for the higher 128bits of the result (0 = higher / 1 = lower 128 bits of source register)
+#define PERMUTE_MASK_2F128(r0, v0, r1, v1) (((r1) << 5) | ((v1) << 4) | ((r0) << 1) | (v0))
--- test/unit/base/sse/Test_maskMacros.cpp
+++ test/unit/base/sse/Test_maskMacros.cpp
@@ -141,3 +141,42 @@ BOOST_AUTO_TEST_CASE(Blend4)
 {
     TestBlend4();
 }
+
+
+
+// Permute ------------------------------------------------------------------------------------------------------------
+
+#ifdef __AVX2__
+
+template <U32 _i = 0, U32 _j = 0, U32 _k = 0, U32 _l = 0>
+void TestPermute2f128()
+{
+    __m256 a = _mm256_setr_ps(1.f, 2.f, 3.f, 4.f, 5.f, 6.f, 7.f, 8.f);
+    __m256 b = _mm256_setr_ps(9.f, 10.f, 11.f, 12.f, 13.f, 14.f, 15.f, 16.f);
+
+    __m256 c = _mm256_permute2f128_ps(a, b, PERMUTE_MASK_2F128(_i, _j, _k, _l));
+
+    for (U32 i = 0; i < 4; ++i)
+    {
+        BOOST_CHECK(sse::GetValue(c, i) == Approx(1.f + i + _i * 8 + _j * 4));
+        BOOST_CHECK(sse::GetValue(c, i + 4) == Approx(1.f + i + _k * 8 + _l * 4));
+    }
+
+
+    constexpr U32 iNext = (_i < 1) ? _i + 1 : 0;
+    constexpr U32 jNext = (iNext == 0) ? (_j < 1) ? _j + 1 : 0 : _j;
+    constexpr U32 kNext = (iNext == 0 && jNext == 0) ? (_k < 1) ? _k + 1 : 0 : _k;
+    constexpr U32 lNext = (iNext == 0 && jNext == 0 && kNext == 0) ? _l + 1 : _l;
+    if constexpr (lNext < 2)
+    {
+        TestPermute2f128<iNext, jNext, kNext, lNext>();
+    }
+}
+
+
+BOOST_AUTO_TEST_CASE(Permute2f128)
+{
+    TestPermute2f128();
+}
+
+#endif // __AVX2__
