5beed8f7174d0816a10bdd8dd0b566e8f8cb04fc
Volker Hirthammer <volker.hirthammer@gmail.com>
2018-11-27 16:20:53 +0100

    Remove upper limits in timer test
    Tests failed because of slow machines and not because of an error
    in the code.

--- test/unit/base/Test_timer.cpp
+++ test/unit/base/Test_timer.cpp
@@ -16,15 +16,15 @@ BOOST_AUTO_TEST_CASE(GetMilliseconds)
     Timer timer;
     std::this_thread::sleep_for(20ms);
     Milliseconds duration = timer.GetElapsedTime<Milliseconds>();
-    BOOST_CHECK(duration >= 20ms && duration <= 25ms);
+    BOOST_CHECK(duration >= 20ms);
 
     timer.Reset();
     std::this_thread::sleep_for(40ms);
     duration = timer.GetElapsedTime<Milliseconds>();
-    BOOST_CHECK(duration >= 40ms && duration <= 45ms);
+    BOOST_CHECK(duration >= 40ms);
 
     timer.Reset();
     std::this_thread::sleep_for(10ms);
     duration = timer.GetElapsedTime<Milliseconds>();
-    BOOST_CHECK(duration >= 10ms && duration <= 15ms);
+    BOOST_CHECK(duration >= 10ms);
 }
