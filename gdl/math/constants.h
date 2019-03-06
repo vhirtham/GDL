b8347c5f0d059ec3170ca4372106a5aefbc23106
Volker Hirthammer <volker.hirthammer@gmail.com>
2019-03-06 16:49:41 +0100

    Add default type to template constants

--- gdl/math/constants.h
+++ gdl/math/constants.h
@@ -1,12 +1,13 @@
 #pragma once
 
+#include "gdl/base/fundamentalTypes.h"
 
 namespace GDL
 {
 
 //! @brief Pi
 //! @tparam _type: Value type
-template <typename _type>
+template <typename _type = F32>
 constexpr _type PI = static_cast<_type>(3.1415926535897932385);
 
 } // namespace GDL
