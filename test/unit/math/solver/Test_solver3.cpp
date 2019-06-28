0c8c47779385215f64c9bfa6b40ebd3075dd0407
Volker Hirthammer <volker.hirthammer@gmail.com>
2019-06-28 14:14:20 +0200

    Add symmetric testcases for 3x3 and 4x4 systems

--- test/unit/math/solver/Test_solver3.cpp
+++ test/unit/math/solver/Test_solver3.cpp
@@ -32,10 +32,12 @@ struct LinearSystem3x3
 class Mat3TestData
 {
     std::vector<LinearSystem3x3> mTests;
+    std::vector<LinearSystem3x3> mSymmetricTests;
 
     Mat3TestData()
     {
         U32 numTestSystems = 0;
+        U32 numSymmetricTestSystems = 0;
 
         std::array<F32, 9> A0 = {{1, 0, 0, 0, 1, 0, 0, 0, 1}};
         std::array<F32, 3> b0 = {{1, 2, 3}};
@@ -46,16 +48,30 @@ class Mat3TestData
         std::array<F32, 3> b1 = {{-2, -1, -2}};
         std::array<F32, 3> x1 = {{3, -3, 1}};
         CreateRowPermutations(A1, b1, x1, mTests, numTestSystems);
+        CreateDiagonalPermutations(A1, b1, x1, mTests, numTestSystems);
 
         std::array<F32, 9> A2 = {{2, 1, 3, 4, 1, 4, 8, 1, 5}};
         std::array<F32, 3> b2 = {{2, 1, 2}};
         std::array<F32, 3> x2 = {{3, -3, 1}};
         CreateRowPermutations(A2, b2, x2, mTests, numTestSystems);
+        CreateDiagonalPermutations(A2, b2, x2, mTests, numTestSystems);
 
         constexpr U32 numPermutations = 3 * 2 * 1;
 
         EXCEPTION(mTests.size() != numTestSystems * numPermutations || numTestSystems == 0,
                   "Testcases are not added as expected");
+
+
+        std::array<F32, 9> S0 = {{4, 2, 4, 2, 10, 5, 4, 5, 9}};
+        std::array<F32, 3> r0 = {{20, 37, 41}};
+        std::array<F32, 3> y0 = {{1, 2, 3}};
+        CreateDiagonalPermutations(S0, r0, y0, mSymmetricTests, numSymmetricTestSystems);
+
+        EXCEPTION(mTests.size() < numPermutations * numSymmetricTestSystems || numSymmetricTestSystems == 0,
+                  "Testcases are not added as expected");
+
+        for (U32 i = 0; i < mSymmetricTests.size(); ++i)
+            CheckSymmetry(mSymmetricTests[i].A);
     }
 
     void CreateRowPermutations(const std::array<F32, 9>& A, const std::array<F32, 3>& b, const std::array<F32, 3>& x,
@@ -68,6 +84,21 @@ class Mat3TestData
                     if (i != j && i != k && j != k)
                         AddRowPermutation(A, b, x, {{i, j, k}}, dataContainer);
     }
+
+
+    void CreateDiagonalPermutations(const std::array<F32, 9>& A, const std::array<F32, 3>& b,
+                                    const std::array<F32, 3>& x, std::vector<LinearSystem3x3>& dataContainer,
+                                    U32& numTestSystems)
+    {
+        ++numTestSystems;
+        for (U32 i = 0; i < 3; ++i)
+            for (U32 j = 0; j < 3; ++j)
+                for (U32 k = 0; k < 3; ++k)
+                    if (i != j && i != k && j != k)
+                        AddDiagonalPermutation(A, b, x, {{i, j, k}}, dataContainer);
+    }
+
+
 
     void AddRowPermutation(const std::array<F32, 9>& A, const std::array<F32, 3>& b, const std::array<F32, 3>& x,
                            const std::array<U32, 3>& permutation, std::vector<LinearSystem3x3>& dataContainer)
@@ -83,11 +114,53 @@ class Mat3TestData
         dataContainer.emplace_back(AP, bP, x);
     }
 
+
+
+    void AddDiagonalPermutation(const std::array<F32, 9>& A, const std::array<F32, 3>& b, const std::array<F32, 3>& x,
+                                const std::array<U32, 3>& permutation, std::vector<LinearSystem3x3>& dataContainer)
+    {
+        std::array<F32, 9> AP;
+        std::array<F32, 3> bP;
+        std::array<F32, 3> xP;
+
+        for (U32 i = 0; i < 3; ++i)
+        {
+            bP[i] = b[permutation[i]];
+            xP[i] = x[permutation[i]];
+            for (U32 j = 0; j < 3; ++j)
+                AP[i + j * 3] = A[permutation[i] + permutation[j] * 3];
+        }
+
+        dataContainer.emplace_back(AP, bP, xP);
+    }
+
+
+    void CheckSymmetry(const std::array<F32, 9>& A)
+    {
+        bool symmetric = true;
+        for (U32 i = 1; i < 3; ++i)
+            for (U32 j = 0; j < i; ++j)
+                symmetric &= A[i + j * 3] == Approx(A[j + i * 3]);
+        EXCEPTION(!symmetric, "Created a non-symmetric matrix for a symmetric testcase!");
+    }
+
+
 public:
-    static const std::vector<LinearSystem3x3>& GetTestData()
+    static const Mat3TestData& Instance()
     {
         static Mat3TestData testData;
-        return testData.mTests;
+        return testData;
+    }
+
+    static const std::vector<LinearSystem3x3>& GetTestData()
+    {
+        return Instance().mTests;
+    }
+
+
+    static const std::vector<LinearSystem3x3>& GetSymmetricTestData()
+    {
+        return Instance().mSymmetricTests;
     }
 };
 
@@ -102,8 +175,8 @@ void TestSolverTestcase(_solver solver, const Matrix& A, const Vector& b, const 
     Vector b2 = A * res;
     for (U32 i = 0; i < 3; ++i)
     {
-        BOOST_CHECK(res[i] == Approx(exp[i], 10));
-        BOOST_CHECK(b2[i] == Approx(b[i], 10));
+        BOOST_CHECK(res[i] == Approx(exp[i], 20));
+        BOOST_CHECK(b2[i] == Approx(b[i], 20));
     }
 }
 
@@ -112,27 +185,38 @@ void TestSolverTestcase(_solver solver, const Matrix& A, const Vector& b, const 
 // --------------------------------------------------------------------------------------------------------------------
 
 template <typename _solver>
-void TestSolver(_solver solver, bool pivot = true)
+void TestSolver(_solver solver, bool pivot = true, bool symmetric = false)
 {
     using Vector = decltype(solver({}, {}));
     using Matrix = typename std::conditional<std::is_same<Vector, Vec3fSSE<true>>::value, Mat3fSSE, Mat3fSerial>::type;
 
-    if (pivot)
+    if (not symmetric)
     {
-        const auto& testcases = Mat3TestData::GetTestData();
-        for (U32 i = 0; i < testcases.size(); ++i)
-            TestSolverTestcase(solver, Matrix(testcases[i].A), Vector(testcases[i].b), Vector(testcases[i].x));
-    }
-    else
-    {
+        if (pivot)
+        {
+            const auto& testcases = Mat3TestData::GetTestData();
+            for (U32 i = 0; i < testcases.size(); ++i)
+                TestSolverTestcase(solver, Matrix(testcases[i].A), Vector(testcases[i].b), Vector(testcases[i].x));
+        }
+        else
+        {
 
-        TestSolverTestcase(solver, Matrix(1, 0, 0, 0, 1, 0, 0, 0, 1), Vector(1, 2, 3), Vector(1, 2, 3));
-        TestSolverTestcase(solver, Matrix(-2, -1, -3, -4, -1, -4, -8, -1, -5), Vector(-2, -1, -2), Vector(3, -3, 1));
-        TestSolverTestcase(solver, Matrix(2, 1, 3, 4, 1, 4, 8, 1, 5), Vector(2, 1, 2), Vector(3, -3, 1));
+            TestSolverTestcase(solver, Matrix(1, 0, 0, 0, 1, 0, 0, 0, 1), Vector(1, 2, 3), Vector(1, 2, 3));
+            TestSolverTestcase(solver, Matrix(-2, -1, -3, -4, -1, -4, -8, -1, -5), Vector(-2, -1, -2),
+                               Vector(3, -3, 1));
+            TestSolverTestcase(solver, Matrix(2, 1, 3, 4, 1, 4, 8, 1, 5), Vector(2, 1, 2), Vector(3, -3, 1));
 
-        Matrix ANeedPivot(0, 1, 0, 1, 0, 0, 0, 0, 1);
-        GDL_CHECK_THROW_DEV(solver(ANeedPivot, Vector()), Exception);
+            Matrix ANeedPivot(0, 1, 0, 1, 0, 0, 0, 0, 1);
+            GDL_CHECK_THROW_DEV(solver(ANeedPivot, Vector()), Exception);
+        }
     }
+
+
+    const auto& testcasesSymmetric = Mat3TestData::GetSymmetricTestData();
+    for (U32 i = 0; i < testcasesSymmetric.size(); ++i)
+        TestSolverTestcase(solver, Matrix(testcasesSymmetric[i].A), Vector(testcasesSymmetric[i].b),
+                           Vector(testcasesSymmetric[i].x));
+
     Matrix AThrow(2, 1, 3, 2, 1, 3, 2, 1, 3);
     GDL_CHECK_THROW_DEV(solver(AThrow, Vector()), Exception);
 }
--- test/unit/math/solver/Test_solver4.cpp
+++ test/unit/math/solver/Test_solver4.cpp
@@ -30,10 +30,12 @@ struct LinearSystem4x4
 class Mat4TestData
 {
     std::vector<LinearSystem4x4> mTests;
+    std::vector<LinearSystem4x4> mSymmetricTests;
 
     Mat4TestData()
     {
         U32 numTestSystems = 0;
+        U32 numSymmetricTestSystems = 0;
 
         std::array<F32, 16> A0 = {{1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1}};
         std::array<F32, 4> b0 = {{1, 2, 3, 4}};
@@ -44,12 +46,27 @@ class Mat4TestData
         std::array<F32, 4> b1 = {{-6, 0, -21, 18}};
         std::array<F32, 4> x1 = {{-1.5, 3, 1, -6}};
         CreateRowPermutations(A1, b1, x1, mTests, numTestSystems);
+        CreateDiagonalPermutations(A1, b1, x1, mTests, numTestSystems);
 
         constexpr U32 numPermutations = 4 * 3 * 2 * 1;
 
         EXCEPTION(mTests.size() < numPermutations * numTestSystems || numTestSystems == 0,
                   "Testcases are not added as expected");
+
+
+        std::array<F32, 16> S0 = {{4, 2, 4, 4, 2, 10, 5, 2, 4, 5, 9, 6, 4, 2, 6, 9}};
+        std::array<F32, 4> r0 = {{36, 45, 65, 62}};
+        std::array<F32, 4> y0 = {{1, 2, 3, 4}};
+        CreateDiagonalPermutations(S0, r0, y0, mSymmetricTests, numSymmetricTestSystems);
+
+        EXCEPTION(mTests.size() < numPermutations * numSymmetricTestSystems || numSymmetricTestSystems == 0,
+                  "Testcases are not added as expected");
+
+        for (U32 i = 0; i < mSymmetricTests.size(); ++i)
+            CheckSymmetry(mSymmetricTests[i].A);
     }
+
+
 
     void CreateRowPermutations(const std::array<F32, 16>& A, const std::array<F32, 4>& b, const std::array<F32, 4>& x,
                                std::vector<LinearSystem4x4>& dataContainer, U32& numTestSystems)
@@ -62,6 +79,23 @@ class Mat4TestData
                         if (i != j && i != k && i != l && j != k && j != l && k != l)
                             AddRowPermutation(A, b, x, {{i, j, k, l}}, dataContainer);
     }
+
+
+
+    void CreateDiagonalPermutations(const std::array<F32, 16>& A, const std::array<F32, 4>& b,
+                                    const std::array<F32, 4>& x, std::vector<LinearSystem4x4>& dataContainer,
+                                    U32& numTestSystems)
+    {
+        ++numTestSystems;
+        for (U32 i = 0; i < 4; ++i)
+            for (U32 j = 0; j < 4; ++j)
+                for (U32 k = 0; k < 4; ++k)
+                    for (U32 l = 0; l < 4; ++l)
+                        if (i != j && i != k && i != l && j != k && j != l && k != l)
+                            AddDiagonalPermutation(A, b, x, {{i, j, k, l}}, dataContainer);
+    }
+
+
 
     void AddRowPermutation(const std::array<F32, 16>& A, const std::array<F32, 4>& b, const std::array<F32, 4>& x,
                            const std::array<U32, 4>& permutation, std::vector<LinearSystem4x4>& dataContainer)
@@ -77,11 +111,54 @@ class Mat4TestData
         dataContainer.emplace_back(AP, bP, x);
     }
 
+
+
+    void AddDiagonalPermutation(const std::array<F32, 16>& A, const std::array<F32, 4>& b, const std::array<F32, 4>& x,
+                                const std::array<U32, 4>& permutation, std::vector<LinearSystem4x4>& dataContainer)
+    {
+        std::array<F32, 16> AP;
+        std::array<F32, 4> bP;
+        std::array<F32, 4> xP;
+
+        for (U32 i = 0; i < 4; ++i)
+        {
+            bP[i] = b[permutation[i]];
+            xP[i] = x[permutation[i]];
+            for (U32 j = 0; j < 4; ++j)
+                AP[i + j * 4] = A[permutation[i] + permutation[j] * 4];
+        }
+
+        dataContainer.emplace_back(AP, bP, xP);
+    }
+
+
+    void CheckSymmetry(const std::array<F32, 16>& A)
+    {
+        bool symmetric = true;
+        for (U32 i = 1; i < 4; ++i)
+            for (U32 j = 0; j < i; ++j)
+                symmetric &= A[i + j * 4] == Approx(A[j + i * 4]);
+        EXCEPTION(!symmetric, "Created a non-symmetric matrix for a symmetric testcase!");
+    }
+
+
+
 public:
-    static const std::vector<LinearSystem4x4>& GetTestData()
+    static const Mat4TestData& Instance()
     {
         static Mat4TestData testData;
-        return testData.mTests;
+        return testData;
+    }
+
+    static const std::vector<LinearSystem4x4>& GetTestData()
+    {
+        return Instance().mTests;
+    }
+
+
+    static const std::vector<LinearSystem4x4>& GetSymmetricTestData()
+    {
+        return Instance().mSymmetricTests;
     }
 };
 
@@ -104,8 +181,8 @@ void TestSolverTestcase(_solver solver, const Matrix& A, const Vector& b, const 
     Vector b2 = A * res;
     for (U32 i = 0; i < 4; ++i)
     {
-        BOOST_CHECK(res[i] == Approx(exp[i], 50));
-        BOOST_CHECK(b2[i] == Approx(b[i], 50));
+        BOOST_CHECK(res[i] == Approx(exp[i], 60));
+        BOOST_CHECK(b2[i] == Approx(b[i], 60));
     }
 }
 
@@ -114,7 +191,7 @@ void TestSolverTestcase(_solver solver, const Matrix& A, const Vector& b, const 
 // --------------------------------------------------------------------------------------------------------------------
 
 template <typename _solver>
-void TestSolver(_solver solver, bool pivot = true)
+void TestSolver(_solver solver, bool pivot = true, bool symmetric = false)
 {
 
     using Vector = decltype(solver({}, {}));
@@ -122,23 +199,31 @@ void TestSolver(_solver solver, bool pivot = true)
             std::is_same<_solver, SSESolverPtr>::value, Mat4fSSE,
             typename std::conditional<std::is_same<_solver, AVXSolverPtr>::value, Mat4fAVX, Mat4fSerial>::type>::type;
 
-
-    if (pivot)
+    if (not symmetric)
     {
-        const auto& testcases = Mat4TestData::GetTestData();
-        for (U32 i = 0; i < testcases.size(); ++i)
-            TestSolverTestcase(solver, Matrix(testcases[i].A), Vector(testcases[i].b), Vector(testcases[i].x));
-    }
-    else
-    {
-        TestSolverTestcase(solver, Matrix(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1), Vector(1, 2, 3, 4),
-                           Vector(1, 2, 3, 4));
-        TestSolverTestcase(solver, Matrix(2, 0, 4, 6, 2, 2, -3, 1, 3, 0, 0, -6, 2, 1, 1, -5), Vector(-6, 0, -21, 18),
-                           Vector(-1.5, 3, 1, -6));
+        if (pivot)
+        {
+            const auto& testcases = Mat4TestData::GetTestData();
+            for (U32 i = 0; i < testcases.size(); ++i)
+                TestSolverTestcase(solver, Matrix(testcases[i].A), Vector(testcases[i].b), Vector(testcases[i].x));
+        }
+        else
+        {
+            TestSolverTestcase(solver, Matrix(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1), Vector(1, 2, 3, 4),
+                               Vector(1, 2, 3, 4));
+            TestSolverTestcase(solver, Matrix(2, 0, 4, 6, 2, 2, -3, 1, 3, 0, 0, -6, 2, 1, 1, -5),
+                               Vector(-6, 0, -21, 18), Vector(-1.5, 3, 1, -6));
 
-        Matrix ANeedPivot(0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);
-        GDL_CHECK_THROW_DEV(solver(ANeedPivot, Vector()), Exception);
+            Matrix ANeedPivot(0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);
+            GDL_CHECK_THROW_DEV(solver(ANeedPivot, Vector()), Exception);
+        }
     }
+
+    const auto& testcasesSymmetric = Mat4TestData::GetSymmetricTestData();
+    for (U32 i = 0; i < testcasesSymmetric.size(); ++i)
+        TestSolverTestcase(solver, Matrix(testcasesSymmetric[i].A), Vector(testcasesSymmetric[i].b),
+                           Vector(testcasesSymmetric[i].x));
+
 
     Matrix AThrow(2, 1, 3, 4, 2, 1, 3, 4, 2, 1, 3, 4, 2, 1, 3, 4);
     GDL_CHECK_THROW_DEV(solver(AThrow, Vector()), Exception);
@@ -269,7 +354,6 @@ BOOST_AUTO_TEST_CASE(TestLUNoPivotSSE)
 
 
 // --------------------------------------------------------------------------------------------------------------------
-
 BOOST_AUTO_TEST_CASE(TestLUPartialPivotSerial)
 {
     SerialSolverPtr solver = Solver::LU<Solver::Pivot::PARTIAL>;
