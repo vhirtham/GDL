3895e701542bcce2ec1efa41f05468960a53fd61
Volker Hirthammer <volker.hirthammer@gmail.com>
2019-07-07 16:28:14 +0200

    Add function traits for std::function

--- gdl/base/functionTraits.h
+++ gdl/base/functionTraits.h
@@ -7,6 +7,7 @@
 #include <cstddef>
 #include <tuple>
 #include <type_traits>
+#include <functional>
 
 namespace GDL
 {
@@ -46,6 +47,37 @@ struct FunctionTraits<_returnType (*)(_args...)>
     };
 };
 
+
+
+//! @brief Struct to extract type information from a std::function
+//! @tparam _returnType: Return type of the function
+//! @tparam _args: Function arguments (parameter pack)
+template <typename _returnType, typename... _args>
+struct FunctionTraits<std::function<_returnType(_args...)>>
+{
+    //! @brief Number of function parameters
+    static const size_t numArguments = sizeof...(_args);
+
+    //! @brief Return type of the function
+    using ReturnType = _returnType;
+
+
+
+    //! @brief Helper struct to extract the argument types
+    //! @tparam _idx: Index of the argument
+    template <size_t _idx>
+    struct Argument
+    {
+        static_assert(_idx < numArguments, "Index exceeds numper of function parameters.");
+
+
+        //! @brief Type of the argument (including reference and cv-qualifier)
+        using Type = typename std::tuple_element<_idx, std::tuple<_args...>>::type;
+
+        //! @brief Value type of the argument (excluding reference and cv-qualifier)
+        using ValueType = typename std::remove_cv<typename std::remove_reference<Type>::type>::type;
+    };
+};
 
 
 } // namespace GDL
--- test/unit/base/Test_functionTraits.cpp
+++ test/unit/base/Test_functionTraits.cpp
@@ -8,10 +8,14 @@
 using namespace GDL;
 
 
+// --------------------------------------------------------------------------------------------------------------------
+
 U32 TestFunc(const I32& a, F32& b, U32 c)
 {
     return static_cast<U32>(a * static_cast<I32>(b)) + c;
 }
+
+
 
 BOOST_AUTO_TEST_CASE(TestFunctionTraits)
 {
@@ -48,11 +52,15 @@ BOOST_AUTO_TEST_CASE(TestFunctionTraits)
 
 
 
+// --------------------------------------------------------------------------------------------------------------------
+
 struct TestStruct
 {
     U32 a = 0;
     F64 b = 1.0;
 };
+
+
 
 template <typename... _args>
 TestStruct TestFuncVariadic(_args...)
@@ -81,6 +89,39 @@ BOOST_AUTO_TEST_CASE(TestFunctionTraits_VariadicFunction)
     BOOST_CHECK(checkFirstArgumentValueType);
 
     constexpr bool checkSecondArgumentType = std::is_same_v<typename FunctionTraits<Func>::Argument<1>::Type, U32>;
+    BOOST_CHECK(checkSecondArgumentType);
+
+    constexpr bool checkSecondArgumentValueType =
+            std::is_same_v<typename FunctionTraits<Func>::Argument<1>::ValueType, U32>;
+    BOOST_CHECK(checkSecondArgumentValueType);
+}
+
+
+
+// --------------------------------------------------------------------------------------------------------------------
+
+BOOST_AUTO_TEST_CASE(TestFunctionTraits_std_function)
+{
+    std::function<U32(const U32&, U32&)> function = [](const U32& a, U32& b) { return a * b; };
+
+    using Func = decltype(function);
+
+    constexpr bool checkReturnType = std::is_same_v<typename FunctionTraits<Func>::ReturnType, U32>;
+    BOOST_CHECK(checkReturnType);
+
+
+    constexpr bool checkNumArguments = FunctionTraits<Func>::numArguments == 2;
+    BOOST_CHECK(checkNumArguments);
+
+    constexpr bool checkFirstArgumentType =
+            std::is_same_v<typename FunctionTraits<Func>::Argument<0>::Type, const U32&>;
+    BOOST_CHECK(checkFirstArgumentType);
+
+    constexpr bool checkFirstArgumentValueType =
+            std::is_same_v<typename FunctionTraits<Func>::Argument<0>::ValueType, U32>;
+    BOOST_CHECK(checkFirstArgumentValueType);
+
+    constexpr bool checkSecondArgumentType = std::is_same_v<typename FunctionTraits<Func>::Argument<1>::Type, U32&>;
     BOOST_CHECK(checkSecondArgumentType);
 
     constexpr bool checkSecondArgumentValueType =
