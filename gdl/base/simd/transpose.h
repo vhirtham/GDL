d56acb78f082d666f08765df91471c821cf094c5
vhirtham <volker.hirthammer@gmail.com>
2020-02-15 17:25:55 +0100

    Add Transpose7x5 function for SIMD registers

--- gdl/base/simd/transpose.h
+++ gdl/base/simd/transpose.h
@@ -1135,6 +1135,21 @@ inline void Transpose6x6(__m256 in0, __m256 in1, __m256 in2, __m256 in3, __m256 
 
 
 
+// 7x5 ----------------------------------------------------------------------------------------------------------------
+
+//! @brief Transposes a 7x5 matrix
+//! @tparam _firstRowIn: Index of the matrix's first row in each register
+//! @tparam _firstRowOut: Index of the output matrix's first row in each register
+//! @tparam _overwriteUnused: Option that specifies if unused values in the output registers can/should be overwritten
+//! @tparam _unusedSetZero: Option that specifies if unused values in the output registers are set to zero
+//! @param in: Input registers
+//! @param out: Output registers
+template <U32 _firstRowIn = 0, U32 _firstRowOut = 0, bool _overwriteUnused = true, bool _unusedSetZero = false>
+inline void Transpose7x5(__m256 in0, __m256 in1, __m256 in2, __m256 in3, __m256 in4, __m256& out0, __m256& out1,
+                         __m256& out2, __m256& out3, __m256& out4, __m256& out5, __m256& out6);
+
+
+
 // 7x6 ----------------------------------------------------------------------------------------------------------------
 
 //! @brief Transposes a 7x6 matrix
